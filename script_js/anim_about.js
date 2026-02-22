de01b39a1101c7349bb2001df23662ed7f404ca7
Miquelvx <72611136+Miquelvx@users.noreply.github.com>
2026-02-22 02:19:56 +0100

    Update pages
    Updating about pages + reworking responsive.

--- script_js/anim_about.js
+++ script_js/anim_about.js
@@ -1,3 +1,16 @@
+// Gestion de l'effet de flou sur la navbar au scroll
+document.addEventListener('scroll', () => {
+    const nav = document.querySelector('.nav');
+    if (nav) {
+        if (window.scrollY > 20) {
+            nav.classList.add('nav-scrolled');
+        } else {
+            nav.classList.remove('nav-scrolled');
+        }
+    }
+});
+
+
 document.addEventListener('DOMContentLoaded', () => {
     const mailBtn = document.getElementById('btn-copy-mail');
     const mailText = document.getElementById('text-mail');
@@ -18,6 +31,30 @@ document.addEventListener('DOMContentLoaded', () => {
                     mailText.textContent = originalText;
                 }, 2000);
             });
+        });
+    }
+
+    // Gestion du clic sur le diplôme TOIEC
+    const toiecDiplome = document.getElementById('diplome-toiec');
+    if (toiecDiplome) {
+        toiecDiplome.addEventListener('click', () => {
+            window.open('https://www.etsglobal.org/fr/en/digital-score-report/CA91F43797D3A78D1EF9499E7AB4446F97090EA5785C01AE808D22AA5D84B0BEMHFkUitMa0d3S2t2bDdvWVlPTmQ0Uk96VHJIRG1Rb2o5TWcwNFNqZjdjc3Bzdi8y', '_blank');
+        });
+    }
+
+    // Gestion du clic sur la certification Azure
+    const AzureCertif = document.getElementById('diplome-azure');
+    if (AzureCertif) {
+        AzureCertif.addEventListener('click', () => {
+            window.open('https://www.credly.com/badges/dcbdefa6-3b1a-4727-86cd-1b4766d44279/linked_in_profile', '_blank');
+        });
+    }
+
+    // Gestion du clic sur la certification AWS
+    const AwsCertif = document.getElementById('diplome-aws');
+    if (AwsCertif) {
+        AwsCertif.addEventListener('click', () => {
+            window.open('https://www.credly.com/badges/185fbba3-e0e8-4fa8-b936-565d8af8847e/public_url', '_blank');
         });
     }
 });
